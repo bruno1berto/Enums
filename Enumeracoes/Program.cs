f84d4fec0bb83a380f45e0d16540eeca1c5beb4d
1bertoMaia <bruno1berto@hotmail.com>
2020-04-10 16:41:22 -0300

    primeiro commit

---
+++ Enumeracoes/Program.cs
@@ -0,0 +1,22 @@
+﻿using Enumeracoes.Entities;
+using System;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = Entities.Enums.OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            Console.ReadLine();
+        }
+    }
+}
